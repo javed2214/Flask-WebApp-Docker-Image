9547e62de8fa8404b5fc447dd20c3b0b3e506bc0
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2020-07-31 23:49:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello():
     return "<h2>Hello World</h2>"
 
-if __name__ == 'main':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', port = 5000, debug = True)
